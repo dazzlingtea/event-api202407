da39a2d9424d167e3c3a21e708215d5b25004232
dazzlingtea <kitsnr08@gmail.com>
2024-07-11 11:01:15 +0900

    # 인증이메일 - 시큐리티 패스워드 인코딩

--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -32,6 +33,9 @@ public class EventUserService {
 
     // 이메일 전송 객체
     private final JavaMailSender mailSender;
+
+    // 패스워드 암호화 객체
+    private final PasswordEncoder passwordEncoder;
 
     // 이메일 중복확인 처리
     public boolean checkEmailDuplicate(String email) {
@@ -160,11 +164,14 @@ public class EventUserService {
         EventUser foundUser = eventUserRepository
                 .findByEmail(dto.getEmail())
                 .orElseThrow(
-                        () -> new RuntimeException("회원 정보가 존재하지 않습니다.")
+                    () -> new RuntimeException("회원 정보가 존재하지 않습니다.")
                 );
 
         // 데이터 반영 (패스워드, 가입시간)
-        foundUser.confirm(dto.getPassword());
+        String password = dto.getPassword();
+        String encodedPassword = passwordEncoder.encode(password); // 암호화
+
+        foundUser.confirm(encodedPassword);
         eventUserRepository.save(foundUser); // 추가정보 담아서 save = update
     }
 }
