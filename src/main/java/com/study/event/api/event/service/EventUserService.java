6b4d7297247b82aa9eddfc48b4676c7d8c0d6445
dazzlingtea <kitsnr08@gmail.com>
2024-07-15 10:55:21 +0900

    # 토큰 발급

--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -1,7 +1,9 @@
 package com.study.event.api.event.service;
 
+import com.study.event.api.auth.TokenProvider;
 import com.study.event.api.event.dto.request.EventUserSaveDto;
 import com.study.event.api.event.dto.request.LoginRequestDto;
+import com.study.event.api.event.dto.response.LoginResponseDto;
 import com.study.event.api.event.entity.EmailVerification;
 import com.study.event.api.event.entity.EventUser;
 import com.study.event.api.event.repository.EmailVerificationRepository;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,6 +39,9 @@ public class EventUserService {
 
     // 패스워드 암호화 객체
     private final PasswordEncoder encoder;
+
+    // 토큰 생성 객체 @Component로 스프링에 등록했기 때문에 주입하여 사용 가능
+    private final TokenProvider tokenProvider;
 
 
     // 이메일 중복확인 처리
@@ -204,7 +210,7 @@ public class EventUserService {
 
 
     // 회원 인증 처리 (login)
-    public void authenticate(final LoginRequestDto dto) {
+    public LoginResponseDto authenticate(final LoginRequestDto dto) {
 
         // 이메일을 통해 회원정보 조회
         EventUser eventUser = eventUserRepository.findByEmail(dto.getEmail())
@@ -227,6 +233,17 @@ public class EventUserService {
 
         // 로그인 성공
         // 인증정보를 어떻게 관리할 것인가?
+        // 인증정보(이메일, 닉네임, 프사, 토큰정보)를 클라이언트에게 전송
+
+        // 토큰 생성
+        String token = tokenProvider.createToken(eventUser);
+
+        return LoginResponseDto.builder()
+                .email(eventUser.getEmail())
+                .role(eventUser.getEmail())
+                .role(eventUser.getRole().toString())
+                .token(token)
+                .build();
 
     }
 
